ec846976ef26a63481207bcdd0ed4ad667882953
tangentepisur4 <sande.jonathan1@gmail.com>
2021-03-20 04:33:49 +0000

    Adding a Views counter to the Page

--- views/js/index.js
+++ views/js/index.js
@@ -2,6 +2,21 @@
 //     document.getElementById("fromEmail").value = "";
 //     document.getElementById("msg").value = "";
 // }
+function countViews(){
+    fetch('https://api.countapi.xyz/update/cv-sande-loh-jonathan/7ff9855a-3ad2-425b-9cd9-ba06f9d3d9f1/?amount=1')
+    .then(res => res.json())
+    .then(res => {
+            document.getElementById("count").innerHTML = res.value;
+    });
+    // Set a new value
+    // "https://api.countapi.xyz/set/cv-sande-loh-jonathan/7ff9855a-3ad2-425b-9cd9-ba06f9d3d9f1?value=NEW-VALUE"
+    // Get info about the key
+    // "https://api.countapi.xyz/info/cv-sande-loh-jonathan/7ff9855a-3ad2-425b-9cd9-ba06f9d3d9f1/"
+
+}
+window.onload = () =>{
+    countViews();
+}
 function pushFunction(){
     alertify.alert('Votre message a bien été envoyé !');
 }
